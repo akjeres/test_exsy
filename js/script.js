d367a057a4cd6eaa74e31801c6f1537cb98f3d71
akjeres <akjeres@gmail.com>
2018-02-19 12:36:53 +0200

    Completed without old browsers compatibility

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
-$(".test").html("test");
+var scroll_delta = 0;
+var position = $(window).scrollTop();
 function parameter_2_int (string) {
 
 	return parseInt(string.substring(0, string.indexOf("px")));
+}
+function getScrollbarWidth() {
+  const outer = document.createElement('div');
+  const inner = document.createElement('div');
+  outer.style.visibility = 'hidden';
+  outer.style.width = '100px';
+  inner.style.width = '100%';
+  outer.appendChild(inner);
+  document.body.appendChild(outer);
+  const widthWithoutScrollbar = outer.offsetWidth;
+  outer.style.overflow = 'scroll';
+  const widthWithScrollbar = inner.offsetWidth;
+  document.body.removeChild(outer);
+
+  return (widthWithoutScrollbar - widthWithScrollbar);
 }
 function clipCarousel() {
 	if ($(window).height() <= $(".carousel-inner").height()) {
@@ -13,7 +29,7 @@ function clipCarousel() {
 			marginTop: ""
 		});
 	}
-	if ($(window).width() < 720) {
+	if ($(window).width() < (720 - getScrollbarWidth())) {
 		if ($(window).height() < $(window).width()) {
 			//$(".header_carousel_wrapper").height($(window).height());
 			$("header").css({
@@ -47,13 +63,73 @@ function arrowJustify() {
 		top: ($(".carousel-inner").height() - $("a.carousel-control-prev").height() - margin_top) / 2 + "px"
 	});
 }
+function navbarCollapse() {
+	if ($(window).width() < (720 - getScrollbarWidth())) { 
+		$("nav").removeClass("navbar-expand-sm").addClass("fixed-top");
+		!($("nav").hasClass("navbar-expand-md")) && $("nav").addClass("navbar-expand-md");
+		$("nav").hasClass("bg-transparent") && $("nav").removeClass("bg-transparent").addClass("bg-light");
+		//alert($('body').innerWidth() + " " + $("body").prop("scrollWidth") + " " + $(window).width());
+	} else {
+		$("nav").hasClass("navbar-expand-md") && $("nav").removeClass("navbar-expand-md");
+		$("nav").addClass("navbar-expand-sm").removeClass("fixed-top");
+		!($("nav").hasClass("bg-transparent")) && $("nav").removeClass("bg-light").addClass("bg-transparent");
+	}
+	
+}
+function navbarJustify() {
+	if ($(window).width() > 1199) {
+		$("nav.navbar").css("left", ($(window).width() - $("nav.navbar").width())/2 + "px");
+	} else {
+		$("nav.navbar").css("left", "");
+	}
+}
+function menuAnimate() {
+	let mobile_flag = $("nav").hasClass("fixed-top");
+	let button_padding = parameter_2_int($("#request_call").css("paddingTop")) +
+		parameter_2_int($("#request_call").css("paddingBottom"));
+	let button_height = $("#request_call").height();
+	let scroll_value = $("nav.navbar").height()
+	+ parameter_2_int($("nav.navbar").css("marginTop"))
+	+ (button_height + button_padding) / 2;
+	let position_flag = (position <= scroll_value);
+	
+	if (!mobile_flag && position_flag) {
+		$("nav.navbar").delay(500).animate({top: 0}, 250);
+	} else $("nav.navbar").css({top: ""});
+
+	return false;
+}
+
+function paddingSet() {
+	console.log(1);
+	let main_offset = $("p.address_paragraph").offset().left;
+	$("#manufacturing h2").css({
+		paddingLeft: main_offset + "px"
+	});
+	$("#manufacturing p").css({
+		paddingLeft: main_offset + "px"
+	});
+}
+$(window).scroll(function() {
+	position = $(window).scrollTop();
+	menuAnimate();
+});
 $(document).ready(function() {
+	//setMobileStyles();
+	menuAnimate();
+	navbarCollapse();
+	navbarJustify();
 	clipCarousel();
 	arrowJustify();
+	paddingSet();
 });
 $(window).resize(function() {
+	//setMobileStyles();
+	navbarJustify();
+	navbarCollapse();
 	clipCarousel();
 	arrowJustify();
+	paddingSet();
 });
 $(".header_carousel a").hover(function() {
 	var t = $(this).prop("className");
@@ -71,6 +147,16 @@ $(".header_carousel a").hover(function() {
 	$("span." + t + "-icon").css({
 		backgroundImage: backgroundImage_url.slice(0, divider_position) + "_unactive.png"
 	});
+});
+$(".smooth_link").click(function() {
+    let elementClick = $(this).attr("href");
+    let nav_padding = parameter_2_int($("#navbar_nav").css("paddingTop")) + 
+    parameter_2_int($("#navbar_nav").css("paddingBottom"));
+    const destination = ($(window).width() < (720 - getScrollbarWidth())) ?
+    ($(elementClick).offset().top - $("#navbar_nav").height() -	nav_padding) : $(elementClick).offset().top;
+    $("html, body").animate({ scrollTop: destination }, 1500);
+
+    return false;
 });
 /*Map*/
 function initMap() {
@@ -96,3 +182,6 @@ function initMap() {
 		        });
 		      }
 /*Map*/
+
+//alert(1000 - getScrollbarWidth());
+//alert(11 - "1");
